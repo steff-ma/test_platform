5f4cad270b048a29d721e10d1ede638ffbd67fd3
steff-ma <steff@foxmail.com>
2020-06-12 15:55:07 +0800

    ready for post

--- automation_platform/urls.py
+++ automation_platform/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('steff/', include('steff.urls'))
+    path('steff/', include('steff.urls')),
 ]
